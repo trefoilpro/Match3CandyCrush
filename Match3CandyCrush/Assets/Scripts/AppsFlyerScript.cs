bdf892560559743702675c3ec0d4932f84330ffe
Roman Protsai <protsayroman228@gmail.com>
2023-05-13 10:28:01 +0300

    some changes in appsflyer

--- Match3CandyCrush/Assets/Scripts/AppsFlyerScript.cs
+++ Match3CandyCrush/Assets/Scripts/AppsFlyerScript.cs
@@ -1,37 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using AppsFlyerSDK;
+using System;
 using UnityEngine;
+using MonoBehaviour = UnityEngine.MonoBehaviour;
 
 public class AppsFlyerScript : MonoBehaviour
 {
-    void OnEnable()
+    private void Start()
     {
-        AppsFlyer.setGCMProjectNumber("YOUR_GCM_PROJECT_NUMBER");
-        AppsFlyer.init("YOUR_DEV_KEY", "YOUR_APP_ID");
-        AppsFlyer.setIsDebug(true);
-        AppsFlyer.loadConversionData("OnConversionDataSuccess", "OnConversionDataFail");
-    }
-
-    void OnDisable()
-    {
-        AppsFlyer.stopSDK();
-    }
-
-    void OnApplicationPause(bool pauseStatus)
-    {
-        if (pauseStatus)
-        {
-            AppsFlyer.onAppPaused();
-        }
-        else
-        {
-            AppsFlyer.onAppResumed();
-        }
-    }
-
-    void OnConversionDataSuccess(Dictionary<string, object> conversionData)
-    {
-        Debug.Log("Successful conversion data received: " + conversionData.ToString());
+        Debug.Log("AppsFlyerScript");
     }
 }
